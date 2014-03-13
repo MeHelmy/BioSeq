581322f983e4a127612e780c75f17b4c76249ae2
medhat <medhat.helmy77@gmail.com>
2014-03-13 14:20:16 +0100

    add new starnd column

--- Entrez/Search/Fasta.py
+++ Entrez/Search/Fasta.py
@@ -4,42 +4,98 @@ Created on Mar 11, 2014
 @author: medhat
 '''
 from Bio import Entrez
-import os
+from urllib2 import HTTPError
+import os , time ,sys , getopt
+
+
+#===============================================================================
+# def main(argv):
+#     try:
+#         opts, args = getopt.getopt(argv, "hi:", ["ifile="])
+#     except getopt.GetoptError:
+#         print 'MapCount.py -i <inputfile>'
+#         sys.exit(2)
+#     for opt, arg in opts:
+#         if opt == '-h':
+#             print 'MapCount.py -i <inputfile>'
+#             sys.exit()
+#         elif opt in ("-i", "--ifile"):
+#             inputFile = arg
+#             lostData = getFasta(inputFile)
+#             printList(lostData)
+#===============================================================================
+  
 Entrez.email = "medhat.helmy77@gmail.com"
-resultFolder = os.path.abspath(__file__) + "/fastaSeq"
+resultFolder = os.path.abspath(__file__)
+resultFolder = os.path.dirname(resultFolder) + "/fastaSeq"
+os.makedirs(resultFolder)
 
 # loop a file of ids and locations
 def getFasta(inputFile):
     try:
         if inputFile:
             fileData = open(inputFile , 'r')
+            missedSequence=[]
             for line in fileData:
                 lineSpliced = line.split('\t')
                 geneId = lineSpliced[0]
-                start = lineSpliced[1]
-                end = lineSpliced[2]
+                start = int(lineSpliced[1])
+                end = int(lineSpliced[2])
                 
                 # get data from ncbi
                 handle = Entrez.esearch(db="nucleotide", term=geneId)
                 record = Entrez.read(handle)
-                recordId = record["IdList"][0]
-                handleSeq = Entrez.efetch(db="nucleotide", id=recordId, rettype="fasta", retmode="text")
-                of = open(resultFolder+"/"+geneId+".txt", "w")
-                fastaData = handleSeq.read()
                 
-                of.write(fastaData)
-                of.close()
-                
+                if not is_empty(record["IdList"]):
+                    recordId = record["IdList"][0]
+                    handleSeq = Entrez.efetch(db="nucleotide", id=recordId, rettype="fasta", retmode="text")
+                    of = open(resultFolder+"/"+geneId+".txt", "w")
+                    fastaData = handleSeq.read()
+                    newSeq = cutSeq(fastaData, start, end)
+                    of.write(newSeq)
+                    time.sleep(1)
+                    of.close()
+                else:
+                    missedSequence.append(geneId)
             
-        
-    except StandardError:
+        return missedSequence
+    except HTTPError:
         print "Oops!  That was ERROR.  Try again..."
         
 # function to cut the data according to the coordinates 
 def cutSeq(seq , start , end):
     try:
         if seq and start and end:
-            return 
+            # overcome the python 0 counting
+            start = start - 1
+            end = end - 1
+            header = seq.split('\n', 1)[0]
+            sansfirstline = '\n'.join(seq.split('\n')[1:])
+            sequenc = sansfirstline[start:end]
+            # if it is - strand get the complimentry
+            content = header +'\n'+sequenc
+            return  content
             
     except StandardError:
-        print "Oops!  That was ERROR cutting sequence.  Try again..."    
+        print "Oops!  That was ERROR cutting sequence.  Try again..."  
+        
+def is_empty(any_structure):
+    if any_structure:
+        #print('Structure is not empty.')
+        return False
+    else:
+        #print('Structure is empty.')
+        return True 
+    
+def printList(data):
+    if data:
+        fo = open("lost.txt","a")
+        for elment in data:
+            fo.write(elment+'\n')
+        fo.close()                
+print("========  BEGINNINGING =========")         
+lost = getFasta("/home/medhat/Samples/merge_test/GenIds.txt")
+print("========  WRITE THE LIST =========")
+printList(lost)
+print("========  DONE =========")
+
--- SeqMonk/Report/MergeReport.py
+++ SeqMonk/Report/MergeReport.py
@@ -28,7 +28,7 @@ def getMergeDataFromFile(inputFile):
         num_lines = sum(1 for row in open(inputFile) if row.rstrip())
         i = 1
         mergedData = {}
-        header = 'GenId\tDiscription\tStart\tEnd'
+        header = 'GenId\tDiscription\tStart\tEnd\tStrand'
         num = 1
         while (num <= num_lines):
             header = header + '\tSample-' + str(num)
@@ -63,6 +63,7 @@ def addToDicFromFile(dictionary , inputFile,i,header):
             disc = colum[8]
             start = colum[2]
             end = colum[3]
+            strand = colum[4]
             if '/' in inputFile:
                 fileName = inputFile.rsplit('/',1)[1]
                 
@@ -71,9 +72,9 @@ def addToDicFromFile(dictionary , inputFile,i,header):
             pValue = colum[5]
             if geneId not in dictionary:
                     if i == 1:
-                        dictionary[geneId] = geneId + '\t' + disc + '\t' + start + '\t' + end + '\t' + samples + '\t' + str(log) + '\t' + pValue
+                        dictionary[geneId] = geneId + '\t' + disc + '\t' + start + '\t' + end + '\t' + strand + '\t' + samples + '\t' + str(log) + '\t' + pValue
                     else:
-                        dictionary[geneId] = geneId + '\t' + disc + '\t' + start + '\t' +  + '\t'  + samples + '\t' + begin + str(log) + '\t' + pValue 
+                        dictionary[geneId] = geneId + '\t' + disc + '\t' + start + '\t' + end + '\t' + strand + '\t' + samples + '\t' + begin + str(log) + '\t' + pValue 
             else:        
                     dictionary[geneId] = dictionary[geneId] +  '\t' + str(log) + '\t' + pValue
         return header           
